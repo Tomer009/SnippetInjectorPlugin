599484ab3c5690195f96cc7879f08a408c9f6744
GitHub Action <action@github.com>
2024-10-03 08:38:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/tomer009/snippetinjectorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tomer009/snippetinjectorplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomer009.snippetinjectorplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomer009.snippetinjectorplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
